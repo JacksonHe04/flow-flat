eb3335cf654ada71af023d91fa425f7f72861efb
何锦诚 <JacksonHe04@outlook.com>
2025-07-01 21:22:33 +0800

    refactor(package): 更新依赖和构建配置
    - 将 custom-react-flow-node 从本地链接改为 npm 版本
    - 更新 rollup 配置以生成 cjs 和 esm 格式的输出文件
    - 修改 package.json 中的构建脚本和模块配置
    - 添加 tslib 作为依赖并更新锁文件

--- package-custom-react-flow-node/rollup.config.js
+++ package-custom-react-flow-node/rollup.config.js
@@ -9,12 +9,12 @@ module.exports = {
   input: 'src/index.ts',
   output: [
     {
-      file: packageJson.main,
+      file: 'dist/index.cjs.js',
       format: 'cjs',
       sourcemap: true,
     },
     {
-      file: packageJson.module,
+      file: 'dist/index.esm.js',
       format: 'esm',
       sourcemap: true,
     },
